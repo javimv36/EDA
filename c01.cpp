5dbdc2a54a551e28471ee23975aea36a207cbac1
Javi <javimv36@gmail.com>
2016-11-26 19:20:09 +0100

    plantilla c01

---
+++ c01.cpp
@@ -0,0 +1,32 @@
+//Author Javi
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX 10000
+
+
+/*
+mplementa una funcion que cumpla la siguiente especi cacion:
+{b >1^n >1}
+fun log(intb,intn)
+return int r
+{b^r-1<n<=b^r}
+*/
+void solve(int A[], int N){
+
+}
+
+int main(int argc, char **argv)
+{
+  int c; //casos a probar
+  int b;
+  int n;
+
+  cin >> c;
+  for (int i=0; i<c; i++){
+    cin >> b;
+    cin >> n;
+    solve(b,n);
+  }
+  return 0;
+}
