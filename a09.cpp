eac80a1d3e4c78a450162c3dc44624e7a4056137
Usuario Local <usuariolocal@localhost.localdomain>
2016-11-29 17:11:07 +0100

    inicio a09, la funcion solve calcula el dinero que gana en d dias. Debe calcular los dias que tarda en calcula n cantidad

---
+++ a09.cpp
@@ -0,0 +1,35 @@
+//Author Javi
+
+/*
+  pseudo code
+
+solve(d){
+ if (d==1||d==2) p+=1;
+ else p+=2*(solve(d-1)*solve(d-2));
+ return p;
+}
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX 10000
+
+int solve(int d){
+ if (d==1||d==2) p+=1;
+ else p+=2*(solve(d-1)*solve(d-2));
+ return p;
+}
+
+int main(int argc, char **argv)
+{
+  int n;
+  int N;
+
+  cin >> n;
+  for (int i=0; i<n; i++){
+    cin >> N;
+    cout << solve(n) << endl;
+  }
+  return 0;
+}
