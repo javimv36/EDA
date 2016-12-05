62ed6714c7adda70460269c872bdfc32eb289065
Usuario Local <usuariolocal@localhost.localdomain>
2016-12-05 12:14:56 +0100

    ejercicio vj02; siempre TRUE, arerglar

---
+++ vj02.cpp
@@ -0,0 +1,41 @@
+//Author Javi
+
+/*
+pseudocode
+
+fun solve(){
+  for (i<p) max=max(m,V[i])
+  for (j=p+1;j<N) min=min(m,V[j])
+  return (max<min);
+}
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX 10000
+
+
+bool solve(int A[], int n, int p){
+  int i, j, M, m;
+  for (i=1, M=A[0];i<=p; i++)M=max(M,A[i]);
+  for (j=p+1; (j<n) && (A[j]>m); j++);
+  return (j==n);
+}
+
+int main(int argc, char **argv)
+{
+  int c; //casos a probar
+  int p;
+  int n;
+  int A[MAX];
+  cin >> c;
+  for (int i=0; i<c; i++){
+    cin >> n;
+    cin >> p;
+    for (int j=0; j<n; j++) cin >> A[j];
+    if (solve(A,n,p)) cout << "SI" << endl;
+    else cout << "NO" << endl;
+  }
+  return 0;
+}
