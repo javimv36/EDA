cfc41eb3150a69e15d49a69c6e7b5585ee14e1b4
Usuario Local <usuariolocal@localhost.localdomain>
2016-12-20 17:58:34 +0100

    ejercicio examen

---
+++ b01_examen.cpp
@@ -0,0 +1,31 @@
+/*
+	fun solve(V[i...j) de enteros, N entero){
+		int n=i+1
+		p=v=0
+		while(n<N-1){
+			if(V[n]>V[n-1]&&V[n]>V[n+1])
+				p++
+			if(V[n]<V[n-1]&&V[n]<V[n+1])
+				v++
+		n++
+		}
+		return p,v
+
+	}
+
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX 10000
+
+void solve(){
+
+}
+
+int main (int argc, char **argv){
+	int V[];
+
+	return 0;
+}
