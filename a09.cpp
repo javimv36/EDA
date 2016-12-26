4e238347e984babdee8788cd23e9abef9a120ff1
javier <javimv36@gmail.com>
2016-12-26 19:40:54 +0100

    eliminada confusion precondicion del problema

--- a09.cpp
+++ a09.cpp
@@ -6,10 +6,6 @@ Problem: Soy rico
 
   pseudo code
 
-solve(d){
- if (d==1||d==2) p+=1;
- else p+=2*(solve(d-1)*solve(d-2));
- return p;
 }
 */
 
@@ -18,10 +14,8 @@ solve(d){
 using namespace std;
 #define MAX 10000
 
-int solve(int d){
- if (d==1||d==2) p+=1;
- else p+=2*(solve(d-1)*solve(d-2));
- return p;
+int solve(){
+ return ;
 }
 
 int main(int argc, char **argv)
