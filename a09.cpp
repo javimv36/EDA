d2fb3fbe28b8d3d8c15ec10c4681db5714688388
Javier Martín <javimv36@gmail.com>
2017-02-01 19:51:53 +0100

    cambio de recursivo a iterativo aceptado por el juez

--- a09.cpp
+++ a09.cpp
@@ -10,23 +10,40 @@ Problem: Soy rico
 */
 
 #include <iostream>
-#include <algorithm>
 using namespace std;
-#define MAX 10000
 
-int solve(){
- return ;
-}
+int isolve (int i);
+int solve(int n);
 
 int main(int argc, char **argv)
 {
   int n;
   int N;
-
   cin >> n;
   for (int i=0; i<n; i++){
     cin >> N;
-    cout << solve(n) << endl;
+    cout << solve(N) << endl;
   }
   return 0;
 }
+
+int solve(int n){
+    int i, s, s1,s2,s0;
+    for(i=0,s=0;s<n;i++){
+        if (i == 0){
+          s2=1;
+          s+=s2;
+        }else if(i==1){
+          s1=1;
+          s+=s1;
+        }
+        else{
+          s0=s1+2*s2;
+          s+=s0;
+          s2=s1;
+          s1=s0;
+        }
+    }
+ return i;
+}
+
