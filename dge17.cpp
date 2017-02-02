c583786d558b3d502c0f7b1bec53215b274614ea
Javi <javimv36@gmail.com>
2017-02-02 20:17:25 +0100

    Plantilla Perfil curva concava

---
+++ dge17.cpp
@@ -0,0 +1,25 @@
+
+//Author Javi
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX 10000
+
+void solve(unsigned long b, unsigned log n){
+
+}
+
+int main(int argc, char **argv)
+{
+  int c; //casos a probar
+  int b;
+  int n;
+
+  cin >> c;
+  for (int i=0; i<c; i++){
+    cin >> b;
+    cin >> n;
+    solve(b,n);
+  }
+  return 0;
+}
