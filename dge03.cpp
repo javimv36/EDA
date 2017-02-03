acfd0a9101acac7293cede9d6bbe6e6b0e7e50aa
Javier Martín <javimv36@gmail.com>
2017-02-03 20:38:31 +0100

    ejercicio funcionando

--- dge03.cpp
+++ dge03.cpp
@@ -5,7 +5,7 @@ Implementar un algoritmo que resuelva el siguiente problema:
 
 P ≡ {0 < n < 10.000}
 fun xxxx (int a[], int n) return int p
-Q ≡ {(0 ≤ p < n) ∧ (menores(a, n, p) ∧ (¬∃j : 0 ≤ j < p : menores(a, n, j)}
+Q ≡ {(0 ≤ p < n) ∧ (menores(a, n, p) ∧ (¬Ej : 0 ≤ j < p : menores(a, n, j)}
 donde menores(a, n, p) ≡ ∀u, w : 0 ≤ u ≤ p < w < n : a[u] < a[w]
 
 Requisitos de implementaci´on.
@@ -32,28 +32,38 @@ Salida de ejemplo
 3
 4
 */
-#include <iostream> 
-#include <algorithm>
+#include <iostream>
 using namespace std;
-#define MAX 100000;
+#define MAX 100000
 
+int min(int a, int b){
+    return (a<b)?a:b;
+}
 
-void solve(int A[], int V[], int N){
-	V[0] = A[0];
-	for(i=1; i< N; i++) V[i] = A[i]+V[i-1];	
+int max(int a, int b){
+   return (a>b)?a:b;
+}
+bool equilibrio(int A[], int n, int p){
+  int M = A[0];
+  int m = A[p+1];
+  for (int i=1;i<=p; i++)M=max(M,A[i]);
+  for (int j=p+2; j<n; j++)m=min(m, A[j]);
+  return M<m;
+}
+int solve(int A[], int N){
+	int p;
+	for(p=0;p<N && !equilibrio(A,N,p);p++);
+	return p;
 }
 
 int main(int argc, char **argv){
-	
-	int V[MAX];
-	int A[MAX];
 	int N;
-	
-	cin>>N;
-	for(int j=0; j<N; j++)cin>>A[i];
-	
-	solve(A, &V, N);
-
-	for(int i=0; i < N; i++)cout<<V[i];
-
+	int n;
+	int V[MAX];
+	cin >> n;
+	for(n;n>0;n--){
+		cin>>N;
+		for(int i=0; i<N; i++)cin>>V[i];
+		cout << solve(V, N) << endl;
+	}
 }
