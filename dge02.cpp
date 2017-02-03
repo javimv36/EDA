42a5a99f9ed4a8b059401b095db0815985d24fcc
Javier Martín <javimv36@gmail.com>
2017-02-03 18:53:53 +0100

    enunciado problema dge02

--- dge02.cpp
+++ dge02.cpp
@@ -1,3 +1,42 @@
+/*
+?????
+Implementar un algoritmo que resuelva el siguiente problema:
+
+P ≡ {0 < n < 10.000 ∧ 0 ≤ p < n}
+fun xxxx (int a[], int n, int p) return bool b
+Q ≡ {b = ∀u, w : 0 ≤ u ≤ p < w < n : a[u] < a[w]}
+
+Requisitos de implementaci´on.
+El orden de complejidad del algoritmo debe ser lineal respecto al n´umero de elementos del vector.
+
+Entrada
+
+La entrada comienza con un valor entero que indica el n´umero de casos de prueba. Cada caso de
+prueba consta de dos l´ıneas. La primera contiene los valores de n y p. La segunda l´ınea contiene los
+valores del vector.
+
+Salida
+
+Para cada caso de prueba se escribe en una l´ınea SI si el resultado de la funci´on es cierto y NO si es
+falso.
+
+Entrada de ejemplo
+4
+15 5
+5 7 3 4 7 3 8 9 8 10 8 9 8 9 10
+4 0
+4 7 5 6
+4 3
+4 3 6 2
+5 2
+5 3 4 6 4
+
+Salida de ejemplo
+SI
+SI
+SI
+NO
+*/
 //Author Javi
 
 /*
