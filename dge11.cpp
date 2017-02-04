9d8c2ea6cb066a2a6df3a9d8cee0898a013762c3
Javier Martín <javimv36@gmail.com>
2017-02-04 18:47:09 +0100

    no funciona nCeros coge valores erroneos

--- dge11.cpp
+++ dge11.cpp
@@ -1,27 +1,26 @@
 /*
-Encuentra la  ́ultima posici ́on de equilibrio
+Encuentra la  ́ultima posicion de equilibrio
 
-Implementa una funci ́on que resuelva el siguiente problemaa:
+Implementa una funcion que resuelva el siguiente problemaa:
 
-P≡{n≥0}funxxxx(inta[],intn)return intp
-Q≡{(−1≤p < n)∧numUnos(a, p) =numCeros(a, p)∧∀u:p < u < n:numU nos(a, u)6=numCeros(a, u)}
-dondenumUnos(a, x)≡#u: 0≤u≤x:a[u] = 1 ynumUnos(a, x)≡#u: 0≤u≤x:a[u] = 0.
+P≡{n≥0} fun xxxx(int a[],int n) return int p
+Q≡{(−1≤p < n) ∧ numUnos(a, p) = numCeros(a, p) ∧ ∀ u:p < u < n:numUnos(a, u)!=numCeros(a, u)}
+dondenumUnos(a, x)≡#u: 0≤u≤x:a[u] = 1 y numUnos(a, x)≡#u: 0≤u≤x:a[u] = 0.
 
-Requisitos de implementaci ́on.Indicar el coste de la soluci ́on obtenida.
-La  funci ́on  que  resuelve  el  problema  debe  recibir  los  datos  en  un
-vector  y  devolver  el  valor  de  laposici ́on (observad que puede ser -1).
+Requisitos de implementacion. Indicar el coste de la solucion obtenida.
+La  funcion  que  resuelve  el  problema  debe  recibir  los  datos  en  un
+vector  y  devolver  el  valor  de  la posicion (observad que puede ser -1).
 
 Entrada
 
-
-La entrada comienza con el n ́umero de casos de prueba.  Cada caso de prueba
-tiene dos l ́ıneas.  En laprimera l ́ınea se indica el tama ̃no del vector, en
+La entrada comienza con el numero de casos de prueba.  Cada caso de prueba
+tiene dos lıneas.  En laprimera lınea se indica el tamaño del vector, en
 la segunda los valores separados por blancos.
 
 Salida
 
-Para cada caso de prueba se escribe en una l ́ınea el valor de la posici ́on
-pedida.  Si el tama ̃no delvector es cero la posici ́on debe ser -1.
+Para cada caso de prueba se escribe en una lınea el valor de la posicion
+pedida.  Si el tamano del vector es cero la posicion debe ser -1.
 
 Entrada de ejemplo
 5
@@ -47,12 +46,19 @@ Salida de ejemplo
 */
 //Author Javi
 #include <iostream>
-#include <algorithm>
 using namespace std;
-#define MAX 10000
+#define MAX 100000
 
-void solve(unsigned long b, unsigned log n){
-
+int solve(int V[], int n){
+  int nCeros, nUnos = 0;
+  int p=-1;
+  for (int i=0;i<n;i++){
+    nCeros+=(V[i]==0);
+    nUnos+=(V[i]==1);
+    cout << "unos: " << nUnos << " ceros: " << nCeros << endl;
+    p=(nUnos==nCeros)?i:p;
+  }
+  return p;
 }
 
 int main(int argc, char **argv)
@@ -63,10 +69,8 @@ int main(int argc, char **argv)
   cin >> N;
   for (int i=0; i<N; i++){
     cin >> n;
-    for (int j=0; j<n; j++){
-      cin >> V[j];
-      solve(V,n);
-    }
+    for (int j=0; j<n; j++)cin >> V[j];
+      cout << solve(V,n) << endl;
   }
   return 0;
 }
