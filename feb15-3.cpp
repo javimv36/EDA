b87d2cfb6b2e4ce9e0d82eb0193880af94d454ea
Javier Martín <javimv36@gmail.com>
2017-02-06 19:27:13 +0100

    ejercicio 3 examen febrero 2015

---
+++ feb15-3.cpp
@@ -0,0 +1,23 @@
+/*
+3. (3,5 puntos) Se dispone de una lista de n productos alimenticios entre los que
+ se quiere escoger algunos para seguir una dieta adecuada.
+ Para cada producto i (0 ≤ i < n) se conoce
+ su precio pi ≥ 0,
+ su contenido en proteinas qi ≥ 0 y
+ su cantidad de calorías ci ≥ 0.
+Se desea seleccionar algunos de estos productos (a lo sumo uno de cada) de forma
+que el precio total no supere un presupuesto M, el contenido proteico total sea
+al menos de Q y el valor calórico sea lo menor posible.
+
+ Diseñar un algoritmo de vuelta atrás para encontrar la selección óptima,
+ es decir, la que minimiza la cantidad de calorías. Se valorarán las podas realizadas
+*/
+//Author Javi
+#include <iostream>
+#define MAX 10000
+using namespace std;
+
+int main(int argc, char **args){
+  
+  return 0;
+}
