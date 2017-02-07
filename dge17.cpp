7d2836e5f2cfe524f2b0bc86b53e18c48f52cc74
Javier Martín <javimv36@gmail.com>
2017-02-07 16:28:45 +0100

    directivas de compilacion añadidas

--- dge17.cpp
+++ dge17.cpp
@@ -1,28 +1,28 @@
 /*
-P ́erfil de una curva c ́oncava
+Perfil de una curva concava
 
-Dados una serie de valores cuyo perfil se ajusta al de una curva concava,
- es decir decrecen hasta undeterminado valor a partir del cual son crecientes,
-  se pide encontrar el valor del mınimo.  Se admiten lıneas estrictamente cre
+  Dados una serie de valores cuyo perfil se ajusta al de una curva concava,
+  es decir decrecen hasta undeterminado valor a partir del cual son crecientes,
+  se pide encontrar el valor del mÄ±nimo.  Se admiten lÄ±neas estrictamente cre
   cientes o estrictamente decrecientes.
 
-  Requisitos de implementaci ́on.
+  Requisitos de implementaci Ìon.
 
   indicar la recurrencia utilizada para el calculo del coste y el coste de la
-  soluci ́on obtenida.La soluci ́on obtenida debe emplear la t ́ecnica de divide
-  y vencer ́asLa funci ́on que resuelve el problema debe recibir los datos en un
-  vector y devolver el valor del m ́ınimo.
+  soluci Ìon obtenida.La soluci Ìon obtenida debe emplear la t Ìecnica de divide
+  y vencer ÌasLa funci Ìon que resuelve el problema debe recibir los datos en un
+  vector y devolver el valor del m ÌÄ±nimo.
 
   Entrada
 
   La entrada consta de una serie de casos de prueba.  Cada caso de prueba consta
-   de dos l ́ıneas, en laprimera se indica el n ́umero de elementos del vector
-   y en la segunda los valores del vector separados porblancos.El n ́umero de
+   de dos l ÌÄ±neas, en laprimera se indica el n Ìumero de elementos del vector
+   y en la segunda los valores del vector separados porblancos.El n Ìumero de
    elementos del vector es mayor que cero.
 
    Salida
 
-   para cada caso de prueba se escribe en una l ́ınea el m ́ınimo de la curva.
+   para cada caso de prueba se escribe en una l ÌÄ±nea el m ÌÄ±nimo de la curva.
 
    Entrada de ejemplo
    5
@@ -45,39 +45,42 @@ Dados una serie de valores cuyo perfil se ajusta al de una curva concava,
 */
 //Author Javi
 #include <iostream>
+#include <algorithm> // min
 using namespace std;
 #define MAX 10000
-#define DYV
+#define DYVONELINE 1
+#define DYV 0
+#define IT 0
+#define REC 0
 
-int min(int a, int b){return (a<b)?a:b;}
-
-//#ifdef DYVONELINE    //solucion divide y vencerás en una linea
-
-int minimoConcava(int V[], int l){
-     return (l==1)?V[0]:((l==2)?(min(V[0],V[1])):((V[(l/2)-1]<V[l/2])?minimoConcava(V, (l/2)):minimoConcava((V+(l/2)), (l-(l/2)))));
+/*
+fun minimoConcava(V,l){
+if (l=1) dev V[0]
+if (l=2) dev min(V);
+if (l=3) dev min(v);
+else
+ if (V[mitad-1] < V[mitad])
+  dev minimoConcava(V, mitad)
+ else
+  dev minimoCincava(Vmitad, mitad)
 }
+*/
 
-#elseif DYV   //solucion divide y venceras
+#if DYVONELINE    //solucion divide y vencerÃ¡s en una linea
+
+  int minimoConcava(int V[], int l){
+       return (l==1)?V[0]:((l==2)?(min(V[0],V[1])):((V[(l/2)-1]<V[l/2])?minimoConcava(V, (l/2)):minimoConcava((V+(l/2)), (l-(l/2)))));
+  }
+
+#elif DYV   //solucion divide y venceras
 
   int minimoConcava(int V[], int l){
     if (l==1)return V[0];
     if (l==2)return min(V[0],V[1]);
-    //if (l==3)return min(V[0],min(V[1],V[2]));
     else return (V[(l/2)-1]<V[l/2])?minimoConcava(V, (l/2)):minimoConcava(V+(l/2), l-(l/2));
   }
-  /*
-  fun minimoConcava(V,l){
-  if (l=1) dev V[0]
-  if (l=2) dev min(V);
-  if (l=3) dev min(v);
-  else
-   if (V[mitad-1] < V[mitad])
-    dev minimoConcava(V, mitad)
-   else
-    dev minimoCincava(Vmitad, mitad)
-  }
-  */
-#elseif REC   //solución recursiva
+
+#elif REC   //solucion recursiva
 
   int minimoConcava(int V[], int l){
     if (l==1)return V[0];
@@ -86,7 +89,7 @@ int minimoConcava(int V[], int l){
     else return min(minimoConcava(V, l/2),minimoConcava(V+d, l-(l/2)));
   }
 
-#else     //solución iterativo
+#elif IT     //solucion iterativo
 
   int minimoConcava(int V[], int l){
     int min = V[0];
@@ -95,8 +98,10 @@ int minimoConcava(int V[], int l){
     }
   return min;
   }
+
 #endif
 
+//CUERPO DEL PROGRAMA
 bool solve(){
   int n;
   int V[MAX];
