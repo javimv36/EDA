dc910fb97275e708dcd5848ac5ee388fdab6c2ec
Usuario Local <Usuario Local>
2017-03-31 18:11:51 +0200

    DuplicarPicos

--- files/tads/lista/mainElimElem.cpp
+++ files/tads/lista/mainElimElem.cpp
@@ -19,24 +19,41 @@ void mostrarLista(List<int>* l){
 	cout << endl;
 }
 
-
+void duplicarPicos(List<int>* l, int& k){
+	List<int>::Iterator it = l->begin();
+	int suma = 0;
+	while (it != l->end()){
+		if (suma == it.elem()){
+			l->insert(it.elem(), it);
+			k++;
+		}
+		suma += it.elem();
+		++it;
+	}
+}
 
 int main(){
-	int n, numCasos, elem;
+	int n, numCasos;
 	cin >> numCasos;
 	for (int i = 0; i < numCasos; i++){
 		List<int>* l = new List<int>();
-		cin >> n;
-		while (n != -1){
+		char c;
+		cin.get(c);
+		c = cin.peek(); // Nos dice el siguiente caracter pero sin consumirlo
+		while (c != '\n'){
+			cin >> n; // como sabemos que no viene el salto de línea leemos el siguiente entero
 			l->push_back(n);
-			cin >> n;
+			c = cin.peek();
 		}
-		cin >> elem;
-		mostrarLista(l);
-		l->removeAll(elem);
+		int k=0;
+		duplicarPicos(l,k);
+		cout << k << ": ";
 		mostrarLista(l);
 		delete l;
 	}
+
+
+	
 	return 0;
 }
 
