36bbfb7c6423468b54c15f7aa2266d97a762956a
alvasenj <alvasenj@ucm.es>
2017-05-02 19:08:24 +0200

    colaInjusta

---
+++ files/tads/colaInjusta/colaInjusta/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include "Queue.h"
+
+using namespace std;
+
+int main(){
+	
+	int pos, a, numCasos, cont = 0;
+
+	cin >> numCasos;
+	while (cont < numCasos) {
+		Queue<int>* cola = new Queue<int>();
+		cin >> a;
+		while(a!= -1){
+			cola->push_back(a);
+			cin >> a;
+		}
+		cont++;
+		cin >> pos;
+		cout << *cola;
+		cola->cola_injusta(*cola, pos);
+		cout << *cola;
+		delete cola;
+	}
+	return 0;
+}
