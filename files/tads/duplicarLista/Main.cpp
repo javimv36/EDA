4eb0ac5d074819a33a46fa0cec0bdd4ce2ba366e
javimv36 <javimv36@gmail.com>
2017-05-03 18:54:30 +0200

    cambios hechos

---
+++ files/tads/duplicarLista/Main.cpp
@@ -0,0 +1,30 @@
+#include "Queue.h"
+#include <iostream>
+using namespace std;
+#include <stdlib.h>
+
+
+bool resuelve(){
+    if(!cin){
+        return false;
+    }else{
+        Queue<int>* cola = new Queue<int>();
+        int n;
+        cin >> n;
+        while(n!=0){
+            cola->push_back(n);
+            cin >> n;
+        }
+        cout << *cola;
+        cola->meDuplico();
+        cout << *cola;
+        delete cola;
+        return true;
+    }
+}
+
+int main()
+{
+    while (resuelve()){}
+    return 0;
+}
