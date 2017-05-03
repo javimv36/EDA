e8898c00119dad3415c0687c3e8ebb74213d6fb6
javimv36 <javimv36@gmail.com>
2017-05-03 17:14:17 +0200

    Ejercicio suma digitos resuelto

--- files/tads/SumaPila/Main.cpp
+++ files/tads/SumaPila/Main.cpp
@@ -6,13 +6,20 @@ using namespace std;
 
 void solve (Stack<int>* pila){
 	int suma=0, elem=0;
-	while (!pila->empty()) {
+	while (pila->size()>1) {
 		elem=pila->top();
 		pila->pop();
 		suma+=elem;
-		cout << elem << " ";
+		cout << elem << " + ";
 	}
-	cout << "=" << suma << "\n";
+	if(!pila->empty()){
+		elem=pila->top();
+		pila->pop();
+		suma+=elem;
+		cout << elem;
+	}	
+
+	cout << " = " << suma << "\n";
 }
 int main() {
 
@@ -20,6 +27,9 @@ int main() {
 	cin >> n;
 	while(n!=-1){
 		Stack<int>* pila = new Stack<int>();
+		if (n==0){
+			pila->push(n);
+		}
 		while (n > 0) {
 			elem = n%10;
 			n/=10;
