fa24847ea84ecb5f7d2304e49d03d310e96c62b2
Usuario Local <Usuario Local>
2017-05-11 19:24:55 +0200

    dehesaPila funcionando pero sin juez

--- files/tads/dehesaPila/Main.cpp
+++ files/tads/dehesaPila/Main.cpp
@@ -6,22 +6,19 @@ using namespace std;
 
 
 int main() {
-	
 	int numVagones;
 	Stack<int> pila;
 	int orden[100000];
 	bool posible = false;
-	int vagon_llegada = 0;
-
 	cin >> numVagones;
-
-	for (int i = 1; i <= numVagones; i++) {
-		cin >> orden[i];
-	}
-	
 	while (numVagones != 0) {
+		int vagon_llegada = 0;
 		pila = Stack<int>();
-		for (int i = 1; i <= numVagones; i++) {
+		bool sacar = true;
+		for (int i = 0; i < numVagones; i++) {
+			cin >> orden[i];
+		}
+		for (int i = 1; (i <= numVagones); i++) {
 			pila.push(i);
 			cout << "Wagon " << i << " in" << endl;
 			if (pila.top() == orden[vagon_llegada]) {
@@ -30,7 +27,16 @@ int main() {
 				vagon_llegada++;
 			}
 		}
-		if(pila.empty() && vagon_llegada == numVagones){
+		while (sacar && vagon_llegada < numVagones){
+			if (pila.top() == orden[vagon_llegada]) {
+				cout << "Wagon " << pila.top() << " out" << endl;
+				pila.pop();
+				vagon_llegada++;
+				sacar = true;
+			}
+			else sacar = false;		
+		}
+		if (pila.empty() && vagon_llegada == numVagones){
 			posible = true;
 		}
 		if (posible) {
@@ -38,9 +44,7 @@ int main() {
 		}
 		else {
 			cout << "IMPOSSIBLE" << endl;
-		}
-		
-		vagon_llegada = 0;
+		}	
 		posible = false;
 		cin >> numVagones;
 	}
