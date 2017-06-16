cf3b8314fd6cc4f56b49673061ac376f77597a77
javimv36 <javimv36@gmail.com>
2017-06-16 18:29:12 +0200

    Fronteras: imprime la lista de los elementos hoja de un arbol binario

--- files/tads/frontera/Main.cpp
+++ files/tads/frontera/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include "Arbin.h"
+#include "List.h"
 
 using namespace std;
 
@@ -19,13 +20,45 @@ Arbin<T> leerArbol(const T &repVacio)
     }
 }
 
-void resuelveCaso()
+List<int> concatenaListas(List<int> l_iz, List<int> l_dr){
+    while(!l_dr.empty()){
+        int e =l_dr.front();
+        l_dr.pop_front();
+        l_iz.push_back(e);
+    }
+    return l_iz;
+}
+List<int> frontera(Arbin<int> a)
 {
-    Arbin<int> arbol;
-    arbol = leerArbol(-1); // -1 es la repr. de arbol vacio
+    List<int> l;
+    if(a.esVacio()){    //si hemos llegado por un nodo sin hijo en uno de los lados
+        //devolver lista vacia
+        return l;
+    }
+    if(a.hijoIz().esVacio()&&a.hijoDr().esVacio()){ //si el nodo es hoja
+        l.push_back(a.raiz());
+        return l;
+    }else{      //si es un nodo con al menos un hijo
+        List<int> l_iz = frontera(a.hijoIz());
+        List<int> l_dr = frontera(a.hijoDr());
+        l = concatenaListas(l_iz, l_dr);
+        return l;
+    }
 }
 
 int main()
 {
-	resuelveCaso();
+    int n;
+    cin >> n;
+    for(int i=0; i<n;i++){
+        Arbin<int> arbol;
+        arbol = leerArbol(-1); // -1 es la repr. de arbol vacio
+	    List<int> frontera_list = frontera(arbol);
+        while(!frontera_list.empty()){  //imprimir lista
+            int e =frontera_list.front();
+            frontera_list.pop_front();
+            cout << e << " ";
+        }
+        cout << endl;
+    }
 }
